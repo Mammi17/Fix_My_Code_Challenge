d83a39852c1951137784628490e9c61ad92dc691
Hadjara <hadjaramamane61@gmail.com>
2023-09-20 01:16:57 +0000

    task 0-3

--- 0x01-challenge/status_server/api/v1/app.py
+++ 0x01-challenge/status_server/api/v1/app.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""
-Web server 
-"""
+"""Web server """
+
 from api.v1.views import app_views
 from flask import Flask, jsonify, make_response
 
@@ -14,7 +13,10 @@ def not_found(error):
     """ json 404 page """
     return make_response(jsonify({"error": "Not found"}), 404)
 
+@app.route('/api/v1/status', methods=["GET"])
+def status():
+    return make_response(jsonify({"OK"}), 200)
 
 if __name__ == "__main__":
-    # python -m api.v1.app 
+    python -m api.v1.app 
     app.run(host="0.0.0.0", port=5000)
